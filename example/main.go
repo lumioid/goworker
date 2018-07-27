94e0bafa9da91c6950485717862d780558af680a
azureru <eru.saputra@gmail.com>
2018-07-27 16:16:29 +0700

    Add Example

---
+++ example/main.go
@@ -0,0 +1,25 @@
+// manually add job:
+// 		redis-cli -r 100 `
+// run the example executables:
+// 		./example -uri=redis://localhost:6379/db -queues="myqueue"
+package main
+
+import (
+	"fmt"
+	"github.com/benmanns/goworker"
+)
+
+func myFunc(queue string, args ...interface{}) error {
+	fmt.Printf("From %s, %v\n", queue, args)
+	return nil
+}
+
+func init() {
+	goworker.Register("MyClass", myFunc)
+}
+
+func main() {
+	if err := goworker.Work(); err != nil {
+		fmt.Println("Error:", err)
+	}
+}
